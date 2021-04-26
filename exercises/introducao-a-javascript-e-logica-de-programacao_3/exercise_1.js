fa808c325cbbb18f8325724d5c9dade5706ff714
Gabriel Takaki <gabitj@icloud.com>
2021-04-26 18:10:33 -0300

    Resolução do exercício 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_1.js
@@ -0,0 +1,13 @@
+let n = 5;
+let linha = '';
+let asterisco = '*';
+
+for(let index = 0; index < n; index += 1) {
+  linha = linha + asterisco;
+};
+
+for(let index = 0; index < n; index += 1) {
+  console.log(linha);
+};
+
+
