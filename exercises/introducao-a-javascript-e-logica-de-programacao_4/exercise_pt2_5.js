8db93dee456bd8c320f800ddf49cb90ce20aeec7
Gabriel Takaki <gabitj@icloud.com>
2021-04-29 15:33:37 -0300

    Resolução dos exercício do dia 5.1

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_pt2_5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercise_pt2_5.js
@@ -1,28 +1,50 @@
 // MOSTRA O NÚMERO COM MAIS REPETIÇÕES
 
-// let numbers = [2, 3, 2, 5, 8, 2, 3];
+let numbers = [2, 3, 2, 5, 8, 2, 3];
 
-// function numbersRep(arr) {
-//     let countRep = 0;
-//     let conutNum = 0;
-//     let indexNumRep = 0;
-//     for(let index in arr) {
-//         let verifyNum = arr[index];
-//         for(let index2 in arr) {
-//             if(verifyNum === arr[index2]) {
-//                 conutNum += 1;
-//             }
-//         }
-//         if(conutNum > countRep) {
-//             countRep = conutNum;
-//             indexNumRep = index;
-//         }
-//         conutNum = 0;
-//     }
-//     return arr[indexNumRep];
-// }
+function numbersRep(arr) {
+    let countRep = 0;
+    let conutNum = 0;
+    let indexNumRep = 0;
+    for(let index in arr) {
+        let verifyNum = arr[index];
+        for(let index2 in arr) {
+            if(verifyNum === arr[index2]) {
+                conutNum += 1;
+            }
+        }
+        if(conutNum > countRep) {
+            countRep = conutNum;
+            indexNumRep = index;
+        }
+        conutNum = 0;
+    }
+    return countRep;
+}
 
-// console.log(numbersRep(numbers))
+console.log(numbersRep(numbers))
+
+// Desafio 11
+function generatePhoneNumber(array) {
+    // seu código aqui
+    // AJUDA EM: https://www.florin-pop.com/blog/2019/04/jcc-create-a-phone-number/ & https://www.w3resource.com/javascript/form/all-numbers.php
+    let allowedNums = /^[0-9]+$/;
+    let phoneNum = `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
+    for (let index = 0; index < array.length; index +=1) {
+  
+      if (array.length > 11) {
+        phoneNum = 'Array com tamanho incorreto.';
+      } else if (array[index] < allowedNums || array[index] > allowedNums) {
+        phoneNum = 'não é possível gerar um número de telefone com esses valores';
+      } else if (k) {
+  
+      } else {
+        return phoneNum;
+      }
+    }
+    return phoneNum;
+  }
+  console.log(generatePhoneNumber([1, 3, 3, 3, 3, 6, 7, 8, 9, 0, 1]));
 
 
 let numbers = [2, 3, 2, 5, 8, 2, 3];
@@ -57,16 +79,17 @@ console.log(numbersRep(numbers));
 
 // MOSTRA QUANTAS VEZES O "2" APARECE
 
-// let numbers1 = [2, 3, 2, 5, 8, 2, 3];
+let numbers1 = [2, 3, 2, 5, 8, 2, 3];
 
-// function numbersRep(arr, repCount) {
-//     let rep = 0;
-//     arr.forEach(element => {
-//         if(element === repCount) {
-//             rep++
-//         };
-//     });
-//     return rep;
-// };
+function numbersRep(arr, repCount) {
+    let rep = 0;
+    arr.forEach(element => {
+        if(element === repCount) {
+            rep++
+        };
+    });
+    return rep;
+};
 
-// console.log(numbersRep(numbers1, 2));
+console.log(numbersRep(numbers1, 2));
+
