893663f0db10b318450e391ec476a9242650fa13
Gabriel Takaki <gabitj@icloud.com>
2021-05-03 19:33:58 -0300

    Resolve exercicios 1-5

---
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -0,0 +1,99 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+
+    for (let index = 0; index < weekDays.length; index += 1) {
+        const days = weekDays[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.innerHTML = days;
+
+        weekDaysList.appendChild(dayListItem);
+    };
+};
+  
+createDaysOfTheWeek();
+  
+// Escreva seu código abaixo.
+
+
+// EXERCICIO 1
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+for (let index = 0; index < dezDaysList.length; index +=1) {
+    let monthDays = dezDaysList[index];
+    let monthDaysLi = document.createElement('li');
+    monthDaysLi.className = 'day';
+    monthDaysLi.innerHTML = monthDays;
+    days.appendChild(monthDaysLi);
+    if (monthDays === 24 || monthDays === 31) {
+        monthDaysLi.className = 'day holiday';
+    } else if (monthDays === 4 || monthDays === 11 || monthDays === 18) {
+        monthDaysLi.className = 'day friday';
+    } else if (monthDays === 25) {
+        monthDaysLi.className = 'day holiday friday';
+    }
+}
+
+const btnContainer = document.querySelector('.buttons-container');
+
+
+// EXERCICIO 2
+
+function holidayDays() {
+    const holidayBtn = document.createElement('button');
+    holidayBtn.innerHTML = 'Feriados'
+    holidayBtn.id = 'btn-holiday';
+    btnContainer.appendChild(holidayBtn);
+}
+
+holidayDays()
+
+
+// EXERCICIO 3
+
+const feriadoBtn = document.getElementById('btn-holiday');
+const daysHoli = document.querySelectorAll('.holiday');
+const primary = 'purple';
+const original = 'rgb(238,238,238)';
+
+feriadoBtn.addEventListener('click', holidayColor);
+
+function holidayColor() {
+    for(let index = 0; index < daysHoli.length; index +=1) {
+        if(daysHoli[index].style.backgroundColor === primary) {
+            daysHoli[index].style.backgroundColor = original;
+        } else {
+            daysHoli[index].style.backgroundColor = primary;
+        }
+    }
+}
+
+
+// EXERCICIO 4
+
+const fridayBtn = document.createElement('button');
+fridayBtn.id = 'btn-friday';
+fridayBtn.innerText = 'SEXTOU!'
+btnContainer.appendChild(fridayBtn);
+
+
+// EXERCICIO 5
+
+const btnFriday = document.querySelector('#btn-friday')
+const fridays = document.querySelectorAll('.friday');
+const msg = 'SEXTOU!!'
+const fridaysArray = [4, 11, 18, 25];
+
+btnFriday.addEventListener('click', itsFriday);
+
+function itsFriday() {
+    for(let index = 0; index < fridays.length; index +=1) {
+        if(fridays[index].innerHTML !== msg) {
+            fridays[index].innerHTML = msg;
+        } else {
+            fridays[index].innerHTML = fridaysArray[index];
+        }       
+    }
+}
+
