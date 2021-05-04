6b6da06b70eb012f072a3bd34be43b104c39a78b
Gabriel Takaki <gabitj@icloud.com>
2021-05-04 19:29:19 -0300

    Resoluçao dos exercicios

--- exercises/javascript-dom-eventos-e-web-storage_4/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_4/script.js
@@ -2,47 +2,93 @@ window.onload = function() {
 
     const body = document.querySelector('body');
     const paragraph = document.querySelectorAll('.paragraph');
-    const divFontSize = document.getElementById('font-size');
-    const divLineHeight = document.getElementById('line-heoght');
-    const divFontFamily = document.getElementById('font-family');
+    // const divFontSize = document.getElementById('font-size');
+    // const divLineHeight = document.getElementById('line-height');
+    // const divFontFamily = document.getElementById('font-family');
 
     function pageColor(color) {
         body.style.backgroundColor = color;
         localStorage.setItem('backgroundColor', color);
     }
 
-    function fontColor(color) {
+    function setFontColor(color) {
         for(let index = 0; index < paragraph.length; index +=1) {
-            paragraph[index].style.backgroundColor = color;
+            paragraph[index].style.color = color;
         }
         localStorage.setItem('fontColor', color);
     }
 
-    function fontSize(size) {
-        for(let index = 0; index < divFontSize.length; index +=1) {
+    function setFontSize(size) {
+        for(let index = 0; index < paragraph.length; index +=1) {
             paragraph[index].style.fontSize = size;
         }
         localStorage.setItem('fontSize', size);
     }
 
-    function lineHeight(height) {
-        for(let index = 0; index < divLineHeight.length; index +=1) {
-            divLineHeight[index].style.lineHeight = height;
+    function setLineHeight(height) {
+        for(let index = 0; index < paragraph.length; index +=1) {
+            paragraph[index].style.lineHeight = height;
         }
         localStorage.setItem('lineHeight', height);
     }
 
-    function fontFamily(font) {
-        for(let index = 0; index < divFontFamily.length; index +=1) {
-            divFontFamily[index].style.fontFamily = font;
+    function setFontFamily(font) {
+        for(let index = 0; index < paragraph.length; index +=1) {
+            paragraph[index].style.fontFamily = font;
         }
         localStorage.setItem('fontFamily', font);
     }
 
-    const backgroundColorButtons = document.querySelectorAll('#background-color>button');
-    for(let index = 0; index < backgroundColorButtons.length; index +=1) {
-        backgroundColorButtons[index].addEventListener('click', (e) => {
+    const backgroundColorBtn = document.querySelectorAll('#background-color>button');
+    for(let index = 0; index < backgroundColorBtn.length; index +=1) {
+        backgroundColorBtn[index].addEventListener('click', (e) => {
             pageColor(e.target.innerHTML);
         });
     }
+
+    const fontColorBtn = document.querySelectorAll('#font-color>button');
+    for(let index = 0; index < fontColorBtn.length; index +=1) {
+        fontColorBtn[index].addEventListener('click', (e) => {
+            setFontColor(e.target.innerHTML);
+        });
+    }
+
+    const fontSizeBtn = document.querySelectorAll('#font-size>button');
+    for(let index = 0; index < fontSizeBtn.length; index +=1) {
+        fontSizeBtn[index].addEventListener('click', (e) => {
+            setFontSize(e.target.innerHTML);
+        });
+    }
+
+    const lineHeightBtn = document.querySelectorAll('#line-height>button');
+    for(let index = 0; index < lineHeightBtn.length; index +=1) {
+        lineHeightBtn[index].addEventListener('click', (e) => {
+            setLineHeight(e.target.innerHTML);
+        });
+    }
+
+    const fontFamilyBtn = document.querySelectorAll('#font-family>button');
+    for(let index = 0; index < fontFamilyBtn.length; index +=1) {
+        fontFamilyBtn[index].addEventListener('click', (e) => {
+            setFontFamily(e.target.innerHTML);
+        });
+    } 
+
+    function setStorage() {
+        const backgroundColor = localStorage.getItem('backgroundColor');
+        if(backgroundColor) pageColor(backgroundColor);
+
+        const fontColor = localStorage.getItem('fontColor');
+        if(fontColor) setFontColor(fontColor);
+
+        const fontSize = localStorage.getItem('fontSize');
+        if(fontSize) setFontSize(fontSize);
+        
+        const lineHeight = localStorage.getItem('lineHeight');
+        if(lineHeight) setLineHeight(lineHeight);
+
+        const fontFamily = localStorage.getItem('fontFamily');
+        if(fontFamily) setFontFamily(fontFamily);
+    }
+    setStorage()
 }
