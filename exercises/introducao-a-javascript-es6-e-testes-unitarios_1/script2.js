ab4c3bcca2c49240692444a6095d67e0527478de
Gabriel Takaki <gabitj@icloud.com>
2021-05-19 18:31:58 -0300

    Exercicio 1 parte 2

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script2.js
@@ -0,0 +1,5 @@
+const factorial = (num) => num > 1 ? num * factorial(num - 1) : 1;
+
+console.log(factorial(4));
+
+// AJUDA EM: https://stackoverflow.com/questions/3959211/what-is-the-fastest-factorial-function-in-javascript
