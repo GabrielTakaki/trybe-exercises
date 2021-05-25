a8f7939c1ad56f9f27d89ef17cd7a61fcc9f5ea0
Gabriel Takaki <gabitj@icloud.com>
2021-05-25 19:33:47 -0300

    Resoluçao do exercicio bonus 1

--- exercises/higher-order-functions-do-javascript-es6.1/exercise2.js
+++ exercises/higher-order-functions-do-javascript-es6.1/exercise2.js
@@ -1,7 +1,7 @@
 const numVerify = (myNum, num) => myNum === num;
 
 const randomNum = (myNum, callback) => {
-  const random = Math.floor((Math.floor() * 5) + 1);
+  const random = Math.floor((Math.random() * 5) + 1);
   return callback(myNum, random) ? 'Parabens voce ganhou!!' : "Tente novamente!";
 }
 
