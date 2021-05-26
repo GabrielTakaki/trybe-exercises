9337097d610d053bc213b5197e114c5a0be4cd8c
Gabriel Takaki <gabitj@icloud.com>
2021-05-26 18:16:57 -0300

    Resoluçao do exercicio 4

--- exercises/higher-order-functions-do-javascript-es6_2/script.js
+++ exercises/higher-order-functions-do-javascript-es6_2/script.js
@@ -63,6 +63,10 @@ const books = [
   },
 ];
 
+
+
+
+// EX1:
 // Adicione o código do exercício aqui:
 function authorBornIn1947() {
   // escreva aqui o seu código
@@ -70,7 +74,13 @@ function authorBornIn1947() {
 }
 
 authorBornIn1947();
+// assert.strictEqual(authorBornIn1947(), 'Stephen King');
 
+
+
+
+
+// EX2:
 function smallerName() {
   let nameBook;
   // escreva aqui o seu código
@@ -85,7 +95,13 @@ function smallerName() {
 }
 
 smallerName();
+// assert.strictEqual(smallerName(), 'Duna');
 
+
+
+
+
+// EX3:
 const expectedResult = {
   author: {
     birthYear: 1948,
@@ -101,9 +117,64 @@ function getNamedBook() {
   // escreva seu código aqui
   return books.find((book) => book.name.length === 26);
 }
+// assert.deepStrictEqual(getNamedBook(), expectedResult);
 
-assert.deepStrictEqual(getNamedBook(), expectedResult);
 
-assert.strictEqual(smallerName(), 'Duna');
-  
-assert.strictEqual(authorBornIn1947(), 'Stephen King');
+
+
+// EX4:
+const expectedResult = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  // escreva aqui seu código
+  return books.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear);
+}
+// assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult);
+
+
+
+
+// EX5:
