f23a712ed4c193d7ac8ea2d30d02c58fd8fb1414
Gabriel Takaki <gabitj@icloud.com>
2021-06-08 20:02:20 -0300

    Refatora para async/await

--- exercises/javascript-assincrono-e-promises_2/promiseExercises.js
+++ exercises/javascript-assincrono-e-promises_2/promiseExercises.js
@@ -1,16 +1,28 @@
-const fetchPromise = () => {
-  const promise = new Promise((resolve, reject) => {
-    const arrRandom = Array.from({ length: 10 }, () => Math.floor(Math.random() * 50));
-    const arrSum = arrRandom.map((number) => number * number)
-    .reduce((prev, curr) => prev + curr);
-    arrSum > 8000 ? reject(arrSum) : resolve(arrSum);
-  });
+const sumArrayNums = () => {
+  const arrRandom = Array.from(
+    { length: 10 },
+    () => Math.floor(Math.random() * 50));
+  const arrSum = arrRandom.map((number) => number * number)
+  .reduce((prev, curr) => prev + curr);
 
-  promise
-    .then(arrSum => [2, 3, 5, 10].map((number) => arrSum / number))
-    .then(array => array.reduce((prev, curr) => prev + curr).toFixed(2))
-    .then(array => console.log(array))
-    .catch(() => console.log(`É mais de oito mil! Essa promise deve estar quebrada!`));
+  if (arrSum > 8000) {
+    throw new Error()
+  }
+
+  return arrSum;
+
+}
+const sumFromDiv = arrSum => [2, 3, 5, 10].map((number) => arrSum / number)
+  .reduce((prev, curr) => prev + curr).toFixed(2);
+
+const fetchPromisse = async () => {
+  try {
+    const sum = await sumArrayNums();
+    const sumFromSum = await sumFromDiv(sum);
+    console.log(sumFromSum)
+  } catch (error) {
+    console.log('É mais de oito mil! Essa promise deve estar quebrada!');
+  }
 }
 
-fetchPromise();
+fetchPromisse();
