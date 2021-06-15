6e765c8da5e41282f4f876fabd44fd60783f971a
Gabriel Takaki <gabitj@icloud.com>
2021-06-15 19:03:26 -0300

    Resoluçao do exercicio 2

--- exercises_module_1/bloco_10/testes-automatizados-com-jest_2/script.test.js
+++ exercises_module_1/bloco_10/testes-automatizados-com-jest_2/script.test.js
@@ -1,7 +1,8 @@
 // const { expect } = require('@jest/globals');
 // const { it } = require('jest-circus');
-const { expect } = require('@jest/globals');
-const { uppercase } = require('./script');
+const { expect, it } = require('@jest/globals');
+// const { describe } = require('yargs');
+const { uppercase, findUserById, getUserName } = require('./script');
 
 
 // EX1:
@@ -13,3 +14,14 @@ describe('verifica se transforma em letra maiuscula', done => {
     });
   });
 });
+
+
+// EX2:
+describe('Verificando resultado da funçao', () => {
+  it('ve se o usauario e existente', () => {
+    expect.assertions(1);
+    return getUserName(4).then(users => {
+      expect(users).toBe('Mark')
+    })
+  })
+})
