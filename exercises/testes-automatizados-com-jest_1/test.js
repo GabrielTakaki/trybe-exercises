14160566bf8094f18ed1e6c2bacf6f84f48e2650
Gabriel Takaki <gabitj@icloud.com>
2021-06-15 13:57:10 -0300

    Exercicios do dia 10.1

--- exercises/testes-automatizados-com-jest_1/test.js
+++ exercises/testes-automatizados-com-jest_1/test.js
@@ -50,8 +50,33 @@ function decode(str) {
   return res;
 }
 
+const techList = (tech, name) => {
+  if (tech.length === 0) return 'Vazio!';
+  const techList = tech
+  .sort()
+  .map((techno) => ({
+    tech: techno,
+    name,
+  }));
+  return techList;
+}
+
+function hydrate(str) {
+  // seu código aqui
+  let sum = 0;
+  let numbers = str.match(/\d+/g).map(Number);
+  // https://stackoverflow.com/questions/53897373/js-how-to-got-the-sum-of-numbers-from-a-string
+  for (let index = 0; index < numbers.length; index+=1) {
+    sum += numbers[index];
+  } if (sum === 1) {
+    return (`${sum} copo de água`);
+  }
+  return (`${sum} copos de água`);
+}
 
 module.exports = {
   decode,
   encode,
+  techList,
+  hydrate,
 };
--- exercises/testes-automatizados-com-jest_1/tests.test.js
+++ exercises/testes-automatizados-com-jest_1/tests.test.js
@@ -1,5 +1,6 @@
-const { decode, encode } = require('./test');
+const { decode, encode, techList, hydrate } = require('./test');
 
+// EX1:
 describe('Testa a função encode e decode', () => {
   it('a função encode é definida', () => {
     expect(encode).toBeDefined();
@@ -49,5 +50,60 @@ describe('Testa a função encode e decode', () => {
   });
   it('checa se o retorno da função tem o mesmo número de caracteres', () => {
     expect(decode('trybe').length).toEqual(5);
+  });
+});
+
+
+
+// EX2:
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
   });
 });
